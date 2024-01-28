22011dc4c14acb4616be728176a3fc3b99c4e2ba
javiermar20000 <129828667+javiermar20000@users.noreply.github.com>
2024-01-28 10:55:32 -0300

    Se añadió LoginSession
    Se añadió logueo de login web usando tokens

---
+++ LoginSession/front-end/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./Home";
+import Register from './Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
